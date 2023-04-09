3036ac5a0252247f09d23aa00bbd9f0901e1119f
Somen Saini <sainisomen@gmail.com>
2023-04-09 11:49:00 -0400

    added you-only-need-one-beginner kata

---
+++ 8kyu/you-only-need-one-beginner.js
@@ -0,0 +1,12 @@
+// You will be given an array a and a value x. All you need to do is check whether the provided array contains the value.
+
+// Array can contain numbers or strings. X can be either.
+
+// Return true if the array contains the value, false if not.
+
+
+// My Solution
+function check(a, x) {
+    const found = a.find(value => value === x)
+    return (found === x) ? true : false
+  }
