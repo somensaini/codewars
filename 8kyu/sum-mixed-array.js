109025bc1de46c25f5139c053221ae37a86b49df
Somen Saini <sainisomen@gmail.com>
2023-05-03 01:11:00 -0400

    added sum-mixed-array kata

---
+++ 8kyu/sum-mixed-array.js
@@ -0,0 +1,10 @@
+// Given an array of integers as strings and numbers, return the sum of the array values as if all were numbers.
+
+// Return your answer as a number.
+
+
+// My Solution
+function sumMix(x){
+    return x.reduce(
+    (a, b) => (Number(a) + Number(b)), 0)
+  }
