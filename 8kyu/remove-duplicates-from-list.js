d9d4494c4d337d2b3f334c10dda4d0eb16cb8e74
Somen Saini <sainisomen@gmail.com>
2023-06-17 12:38:45 -0400

    added remove-duplicates-from-list kata

---
+++ 8kyu/remove-duplicates-from-list.js
@@ -0,0 +1,8 @@
+// Define a function that removes duplicates from an array of numbers and returns it as a result.
+
+// The order of the sequence has to stay the same.
+
+// My Solution
+function distinct(a) {
+    return Array.from(new Set(a))
+  }
