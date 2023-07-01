4bccdb9094176eb090e1b5a7c1095fca7bf95e6e
Somen Saini <sainisomen@gmail.com>
2023-07-01 12:12:21 -0400

    added enumerable-magic-does-my-list-include-this kata

---
+++ 8kyu/enumerable-magic-does-my-list-include-this.js
@@ -0,0 +1,4 @@
+// Create a method that accepts a list and an item, and returns true if the item belongs to the list, otherwise false.
+
+// My Solution
+const include = (arr, item) => arr.includes(item)
