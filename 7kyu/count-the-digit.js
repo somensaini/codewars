0be3f1491871d9d4530499f558fb6053c0868528
Somen Saini <sainisomen@gmail.com>
2023-07-01 12:40:22 -0400

    added count-the-digit kata

---
+++ 7kyu/count-the-digit.js
@@ -0,0 +1,18 @@
+// Take an integer n (n >= 0) and a digit d (0 <= d <= 9) as an integer.
+
+// Square all numbers k (0 <= k <= n) between 0 and n.
+
+// Count the numbers of digits d used in the writing of all the k**2.
+
+// Implement the function taking n and d as parameters and returning this count.
+
+// My Solution
+function nbDig(n, d) {
+    let k = ""
+    
+    for (let i = 0; i <= n; i++){
+      k += i**2 + " "
+    }
+    
+    return k.split('').filter(number => number.match(d)).length
+  }
