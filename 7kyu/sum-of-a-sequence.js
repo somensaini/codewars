1e0f32260d23adf940f8799bc352f430fd507b4a
Somen Saini <sainisomen@gmail.com>
2023-07-03 15:39:58 -0400

    added sum-of-a-sequence kata

---
+++ 7kyu/sum-of-a-sequence.js
@@ -0,0 +1,14 @@
+// Your task is to write a function which returns the sum of a sequence of integers.
+
+// The sequence is defined by 3 non-negative values: begin, end, step.
+
+// If begin value is greater than the end, your function should return 0. If end is not the result of an integer number of steps, then don't add it to the sum. See the 4th example below.
+
+// My Solution
+const sequenceSum = (begin, end, step) => {
+    let count =  0
+    for (let i = begin; i <= end; i+= step){
+      count += i
+    }
+    return count
+  };
