119095174a0a93c4d07c437a36d73bd5b456da9d
Somen Saini <sainisomen@gmail.com>
2023-07-13 15:04:37 -0400

    added area-of-a-square kata

---
+++ 8kyu/area-of-a-square.js
@@ -0,0 +1,6 @@
+// Complete the function that calculates the area of the red square, when the length of the circular arc A is given as the input. Return the result rounded to two decimals.
+
+// My Solution
+function squareArea(A){
+  return (2 * A / Math.PI) ** 2
+}
