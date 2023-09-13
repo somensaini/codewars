338b25043360644e9640f56eab73eb825f707ec0
Somen Saini <sainisomen@gmail.com>
2023-09-13 12:00:00 -0400

    added reverse-words kata

---
+++ 7kyu/reverse-words.js
@@ -0,0 +1,6 @@
+// Complete the function that accepts a string parameter, and reverses each word in the string. All spaces in the string should be retained.
+
+// My Solution
+function reverseWords(str) {
+    return str.split('').reverse().join('').split(' ').reverse().join(' ')
+  }
