28a86ab184f951745f32e8657ecaf00364ce6a40
Somen Saini <sainisomen@gmail.com>
2023-09-14 12:00:00 -0400

    added odd-or-even kata

---
+++ 7kyu/odd-or-even.js
@@ -0,0 +1,12 @@
+// Task:
+
+// Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+
+// My Solution
+function oddOrEven(array) {
+    return (array.reduce((acc, cur) => (acc + cur), 0) % 2 == 0) ? "even" : "odd"
+  }
