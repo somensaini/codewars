7a6539c42c5d881c4a005ec2952c7efc71b0a2f8
Somen Saini <sainisomen@gmail.com>
2024-01-24 03:55:00 -0500

    added find-the-missing-letter kata

---
+++ 6kyu/find-the-missing-letter.js
@@ -0,0 +1,15 @@
+// Find the missing letter
+
+// Write a method that takes an array of consecutive (increasing) letters as input and that returns the missing letter in the array.
+
+// You will always get an valid array. And it will be always exactly one letter be missing. The length of the array will always be at least 2.
+// The array will always contain letters in only one case.
+
+function findMissingLetter(array) {
+  let first = array[0].charCodeAt(0)
+  for (let i = 1; i < array.length; i++) {
+    if (first + i !== array[i].charCodeAt(0)) {
+      return String.fromCharCode(first + i)
+    }
+  }
+}
