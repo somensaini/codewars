19aaa205b67e3d4177c68b29561e6ffb3aed3592
Somen Saini <sainisomen@gmail.com>
2024-02-22 05:46:00 -0500

    added calculating-with-functions kata

---
+++ 5kyu/calculating-with-functions.js
@@ -0,0 +1,22 @@
+// This time we want to write calculations using functions and get the results. Let's have a look at some examples:
+
+// seven(times(five())); // must return 35
+// four(plus(nine())); // must return 13
+// eight(minus(three())); // must return 5
+// six(dividedBy(two())); // must return 3
+
+const zero = (a) => (a ? a(0) : 0)
+const one = (a) => (a ? a(1) : 1)
+const two = (a) => (a ? a(2) : 2)
+const three = (a) => (a ? a(3) : 3)
+const four = (a) => (a ? a(4) : 4)
+const five = (a) => (a ? a(5) : 5)
+const six = (a) => (a ? a(6) : 6)
+const seven = (a) => (a ? a(7) : 7)
+const eight = (a) => (a ? a(8) : 8)
+const nine = (a) => (a ? a(9) : 9)
+
+const plus = (a) => (b) => a + b
+const minus = (a) => (b) => b - a
+const dividedBy = (a) => (b) => Math.floor(b / a)
+const times = (a) => (b) => a * b
