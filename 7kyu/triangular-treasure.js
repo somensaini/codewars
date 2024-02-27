d8201661793fd431dc4c5bf4089825f3dfbb54bf
Somen Saini <sainisomen@gmail.com>
2024-02-27 05:57:00 -0500

    added triangular-treasure kata

---
+++ 7kyu/triangular-treasure.js
@@ -0,0 +1,31 @@
+// Triangular numbers are so called because of the equilateral triangular shape that they occupy when laid out as dots. i.e.
+
+// 1st (1)   2nd (3)    3rd (6)
+// *          **        ***
+//            *         **
+//                      *
+
+// You need to return the nth triangular number. You should return 0 for out of range values:
+
+// For example: (Input --> Output)
+
+// 0 --> 0
+// 2 --> 3
+// 3 --> 6
+// -10 --> 0
+
+function triangular(n) {
+  //   PREP
+  //   Parameters
+  //   - A number
+  //   Return
+  //   - Return the nth triangular number, 0 for out of range numbers
+  //   Examples
+  //   0 : 0
+  //   2 : 3
+  //   3 : 6
+  //   -10 : 0
+  //   Pseudocode
+  //   - Return n * (n + 1) / 2
+  return n > 0 ? (n * (n + 1)) / 2 : 0
+}
