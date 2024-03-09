eb639cd43662978f94b4dabf30698994bcf5dbcc
Somen Saini <sainisomen@gmail.com>
2024-03-09 04:09:00 -0500

    added predict-your-age kata

---
+++ 7kyu/predict-your-age.js
@@ -0,0 +1,32 @@
+// My grandfather always predicted how old people would get, and right before he passed away he revealed his secret!
+
+// In honor of my grandfather's memory we will write a function using his formula!
+
+//     Take a list of ages when each of your great-grandparent died.
+//     Multiply each number by itself.
+//     Add them all together.
+//     Take the square root of the result.
+//     Divide by two.
+
+// Example
+
+// predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+
+// Note: the result should be rounded down to the nearest integer.
+
+function predictAge(age1, age2, age3, age4, age5, age6, age7, age8) {
+  //   PREP
+  //   Parameters
+  //   - A list of integers
+  //   Return
+  //   - Each age multiplied by itself
+  //   - Added all together
+  //   - The square root of the result
+  //   - Divided by two
+  //   - Rounded down to the nearest integer
+  //   Examples
+  //   - predictAge(65, 60, 75, 55, 60, 63, 64, 45) === 86
+  let arr = [age1, age2, age3, age4, age5, age6, age7, age8]
+  let sum = arr.reduce((acc, cur) => acc + cur * cur, 0)
+  return Math.floor(Math.sqrt(sum) / 2)
+}
