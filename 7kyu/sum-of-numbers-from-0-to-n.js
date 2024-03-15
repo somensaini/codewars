b6e23cd5caa40296aa918980b8d94107cd748e6a
Somen Saini <sainisomen@gmail.com>
2024-03-15 05:11:00 -0400

    added sum-of-numbers-from-0-to-n kata

---
+++ 7kyu/sum-of-numbers-from-0-to-n.js
@@ -0,0 +1,27 @@
+// Description:
+
+// We want to generate a function that computes the series starting from 0 and ending until the given number.
+
+var SequenceSum = (function () {
+  function SequenceSum() {}
+
+  SequenceSum.showSequence = function (count) {
+    var sum = 0
+    var str = ""
+    if (count === 0) {
+      return "0=0"
+    } else if (count < 0) {
+      return count + "<0"
+    } else {
+      for (var i = 0; i < count; i++) {
+        sum += i
+        str += i + "+"
+      }
+      sum += count
+      str += count + " = " + sum
+      return str
+    }
+  }
+
+  return SequenceSum
+})()
