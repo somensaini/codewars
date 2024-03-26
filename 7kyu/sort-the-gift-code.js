58d7ceb5b35b0997ff521becb8c863c08352c425
Somen Saini <sainisomen@gmail.com>
2024-03-26 15:39:52 -0400

    added sort-the-gift-code kata

---
+++ 7kyu/sort-the-gift-code.js
@@ -0,0 +1,7 @@
+// Sort the Gift Code
+
+// Write a function called sortGiftCode/sort_gift_code/SortGiftCode that accepts a string containing up to 26 unique alphabetical characters, and returns a string containing the same characters in alphabetical order.
+
+function sortGiftCode(code) {
+  return code.split("").sort().join("")
+}
